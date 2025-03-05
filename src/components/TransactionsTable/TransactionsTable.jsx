961e584a71af814cea198fbe36c8fd14d88ea7a1
rohitgalande1032 <rohitg3234@gmail.com>
2025-03-05 21:29:08 +0530

    Sort transactions by date and amount

--- src/components/TransactionsTable/TransactionsTable.jsx
+++ src/components/TransactionsTable/TransactionsTable.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react'
 import "./styles.css"
-import { Table } from 'antd'
+import { Radio, Select, Table } from 'antd'
 
 const TransactionsTable = ({transactions}) => {
   const [search, setSeach] = useState("")
+  const [typeFilter, setTypeFilter] = useState("")
+  const [sortKey, setSortKey] = useState("")
 
   const columns = [
     {
@@ -36,12 +38,45 @@ const TransactionsTable = ({transactions}) => {
 
   //Filter transactions by name
   let filteredTransactions = transactions.filter((transaction) => 
-    transaction.name.toLowerCase().includes(search.toLocaleLowerCase())
+    transaction.name.toLowerCase().includes(search.toLocaleLowerCase()) && transaction.type.includes(typeFilter)
   )
+
+  //Sort transactions by date and amount
+  let sortedTransactions = [...filteredTransactions].sort((a, b) => {
+    if(sortKey === "date") {
+      return new Date(a.date) - new Date(b.date);
+    }else if(sortKey === "amount") {
+      return a.amount - b.amount
+    } else {
+      return 0
+    }
+  })
+
   return (
     <>
     <input placeholder='Search by Name' value={search} onChange={(e)=>setSeach(e.target.value)}/>
-    <Table dataSource={filteredTransactions} columns={columns} />
+
+    <Select 
+    className='select-input'
+    onChange={(value)=>setTypeFilter(value)}
+    value={typeFilter}
+    placeholder="Filter"
+    allowClear>
+      <Option value="">All</Option>
+      <Option value="income">Income</Option>
+      <Option value="expense">Expense</Option>
+    </Select>
+
+    <Radio.Group
+      className='input-value'
+      onChange={(e)=> setSortKey(e.target.value)}
+      value={sortKey}
+      >
+        <Radio.Button value="">No Sort</Radio.Button>
+        <Radio.Button value="date">Sort by Date</Radio.Button>
+        <Radio.Button value="amount">Sort by Amount</Radio.Button>
+      </Radio.Group>
+    <Table dataSource={sortedTransactions} columns={columns} />
     </>
   )
 }
