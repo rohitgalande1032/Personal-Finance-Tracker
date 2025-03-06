bf2dd128918491d1f2455722c37820152b78cbad
rohitgalande1032 <rohitg3234@gmail.com>
2025-03-06 18:15:08 +0530

    add pie chart

--- src/components/Charts/Charts.jsx
+++ src/components/Charts/Charts.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Line } from '@ant-design/plots'
+import { Line, Pie } from '@ant-design/plots'
 import "./styles.css"
 
 const Charts = ({sortedTransactions}) => {
@@ -7,24 +7,38 @@ const Charts = ({sortedTransactions}) => {
         return {date: item.date, amount: item.amount}
     })
 
+    const spendingData = sortedTransactions
+    .filter(transaction => transaction.type === "expense") // Filter correctly
+    .map(transaction => ({ tag: transaction.tag, amount: transaction.amount })); 
+
+    let finalSpendings = spendingData.reduce((acc, obj) => {
+        let key = obj.tag;
+        if(!acc[key]){
+            acc[key] = {tag: obj.tag, amount: obj.amount}
+        } else{
+            acc[key].amount += obj.amount
+        }
+        return acc
+    }, {})
+
     const config = {
-        data,
+        data:data,
         width: 800,
         height: 400,
-        autoFit: false,
+        autoFit: true,
         xField: "date",
         yField: "amount",
-        point: {
-            size: 5,
-            shape: "diamond"
-        },
-        label: {
-            style: {
-                fill: "#aaa"
-            }
-        }
+    }
+
+    const spendingConfig = {
+        data: Object.values(finalSpendings),
+        width: 500,
+        height: 350,
+        angleField: "amount",
+        colorField: "tag"
     }
     let chart
+    let pieChart
 
   return (
     <div className='charts-wrapper'>
@@ -35,6 +49,7 @@ const Charts = ({sortedTransactions}) => {
 
         <div>
             <h2>Your Spendings</h2>
+            <Pie {...spendingConfig} onReady={(chartInstance) => (pieChart=chartInstance)} />
         </div>
     </div>
   )
